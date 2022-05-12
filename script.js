8e7a7b22c84934280d2fa25d41d5ead19239ecca
Brian Schmeling <brianschmeling2@gmail.com>
2022-05-12 16:21:19 -0400

    Create functionality for winning conditions

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const gameBoard = document.querySelector("#game-board")
 // console.log(square)
 // console.log(gameBoard)
+
+let counter = 1
+let winArr = ["0","0","0","0","0","0","0","0","0"]
+
 function createGrid() {
+    let arrPlace = 0
     for (i = 1; i < 4; i++) {
         for (j = 1; j < 4; j++) {
             const createDiv = document.createElement("div")
             createDiv.style.cssText = `
             grid-row: ${[i]};
             grid-column: ${[j]};
-            background-color: white;
             border: 5px black solid;
-            background-color: white;
-            `
+            background-color: white;`
             createDiv.setAttribute("id", `grid${[i]}${[j]}`)
             createDiv.setAttribute("class", "square")
+            createDiv.setAttribute("value", arrPlace)
+            arrPlace++
             gameBoard.appendChild(createDiv)
             // console.log(createDiv)
         }
@@ -25,29 +30,32 @@ createGrid()
 const squares = document.querySelectorAll(".square")
 const playerTurn = document.querySelector("#player-turn")
 
-let counter = 1
 
 squares.forEach(square => {
     square.addEventListener("click", function chooseSquare() {
         const id = square.getAttribute("id")
         const choice = document.querySelector(`#${id}`)
-        // console.log(choice)
+        // console.log(choice.getAttribute("value"))
         if(choice.style.backgroundColor == "white") {
             if ((counter % 2) === 0) {
                 choice.style.cssText = `
                 background: red;
                 border: 5px black solid;`
                 playerTurn.innerText = "Blue's turn!"
+                winArr.splice(choice.getAttribute("value"), 1, "red")
+                // console.log(choice.getAttribute("value"))
             } else {
                 choice.style.cssText = `
                 background: blue;
                 border: 5px black solid;`
                 playerTurn.innerText = "Red's turn!"
+                winArr.splice(choice.getAttribute("value"), 1, "blue")
             }
             counter++
         } else {
             alert("Make another choice!")
         }
+        checkWinner()
     })
 })
 
@@ -62,6 +70,36 @@ function resetGame() {
     }
     counter = 1
     playerTurn.innerText = "Blue's turn!"
+    winArr = ["0","0","0","0","0","0","0","0","0"]
 }
 
 resetBtn.addEventListener("click", resetGame)
+
+
+
+function checkWinner() {
+    if (winArr[0] === "red" && winArr[1] === "red" && winArr[2] === "red" ||
+    winArr[3] === "red" && winArr[4] === "red" && winArr[5] === "red" ||
+    winArr[6] === "red" && winArr[7] === "red" && winArr[8] === "red" ||
+    winArr[0] === "red" && winArr[3] === "red" && winArr[6] === "red" ||
+    winArr[1] === "red" && winArr[4] === "red" && winArr[7] === "red" ||
+    winArr[2] === "red" && winArr[5] === "red" && winArr[8] === "red" ||
+    winArr[0] === "red" && winArr[4] === "red" && winArr[8] === "red" ||
+    winArr[2] === "red" && winArr[4] === "red" && winArr[6] === "red"
+    ){
+        alert("Red Wins!")
+        // resetGame()
+    } else if (winArr[0] === "blue" && winArr[1] === "blue" && winArr[2] === "blue" ||
+    winArr[3] === "blue" && winArr[4] === "blue" && winArr[5] === "blue" ||
+    winArr[6] === "blue" && winArr[7] === "blue" && winArr[8] === "blue" ||
+    winArr[0] === "blue" && winArr[3] === "blue" && winArr[6] === "blue" ||
+    winArr[1] === "blue" && winArr[4] === "blue" && winArr[7] === "blue" ||
+    winArr[2] === "blue" && winArr[5] === "blue" && winArr[8] === "blue" ||
+    winArr[0] === "blue" && winArr[4] === "blue" && winArr[8] === "blue" ||
+    winArr[2] === "blue" && winArr[4] === "blue" && winArr[6] === "blue"
+    ){
+        alert("Blue Wins!")
+    } else if (winArr[0] !== "0" && winArr[1] !== "0" && winArr[2] !== "0" && winArr[3] !== "0" && winArr[4] !== "0" !== "0" && winArr[5] !== "0" && winArr[6] !== "0" && winArr[7] !== "0" && winArr[8] !== "0") {
+        alert("It's a tie!")
+    }
+}
